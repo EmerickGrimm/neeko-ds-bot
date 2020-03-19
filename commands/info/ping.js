2fd8e309872884ac64bae1150f290cc4d29633d1
EmerickGrimm <62389455+EmerickGrimm@users.noreply.github.com>
2020-03-19 21:09:34 +0200

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -3,6 +3,7 @@ module.exports = {
     category: "info",
     description: "Возвращает пинг бота",
     run: async (bot, message, args) =>{
+        await message.delete().catch (O_o=>{});
         const msg = await message.channel.send(`🔮 Пингую ... `);
 
         msg.edit(`💁🏼 Задержка: ${Math.floor(msg.createdAt - message.createdAt)}ms`);

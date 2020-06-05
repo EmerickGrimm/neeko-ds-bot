6fb21b074f01c296b887403e86882ed9b148fd0e
Dmytry Vynarchyk <dmytry.vynarchuk@yandex.com>
2020-06-05 20:12:52 +0300

    API Endpoint check added

--- commands/info/isalive.js
+++ commands/info/isalive.js
@@ -4,10 +4,21 @@ module.exports = {
     description: "Возвращает пинг бота до других серверов (использующихся в боте)",
     run: async (bot, message, args) =>{
         var tcpp = require('tcp-ping');
+        const superagent = require("snekfetch");
+
         await message.delete().catch (O_o=>{});
 
         tcpp.ping({ address: 'nekos.life', port: 80 }, function(err, data) {
             console.log(data);
         });
-    }
-} 
+
+
+        superagent.get('https://nekos.life/api/v2/endpoints')
+        .end((err, response) => {
+        
+
+          console.log(`Neko's endpoint result: ${response.body}`);
+
+    })
+}
+}
